30f62693b372209171f410eb161e03e8e1919833
Milena Santana <milenasantanamtv@gmail>
2021-12-30 18:47:42 -0300

    Comecando trabalho pratico

--- tp/trabalho/jogo.c
+++ tp/trabalho/jogo.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 const int max=10;
 char mat[10][10], palavra[], encontrada[];
@@ -8,9 +5,8 @@ int tam = 0;
 int op,opPesquisa,i,j,k,l,m,n,o,p,q,r,s,t;
 int *pos1,*pos2;
 
-void menuPrincipal();
 void menuPrincipal()
-{	
+{
     printf("\n\n\t================MENU================\n");
     printf("\t1 - Cria Matriz a partir do teclado\n");
     printf("\t2 - Imprime a Matriz\n");
@@ -19,12 +15,10 @@ void menuPrincipal()
     printf("\t5 - Executa teste padrao\n");
 	printf("\t0 - Sair do Programa\n\n");
 	printf("\tEscolha uma opcao: ");
-    
-
     scanf("%d", &op);
-    
+
 	switch (op){
-        
+
 		case 1:
 			leia();
 		break;
@@ -49,16 +43,16 @@ void menuPrincipal()
         	system("cls");
             printf("\n\tSaindo...\n");
             return 0;
-        break; 
+        break;
 
         default:
             printf("\nValor invalido\n");
         break;
-    }	
+    }
 }
 
-void leia();
 void leia(){
+
     system("cls");
     printf("\n\tInsira o tamanho da matriz quadrada(M x M): ");
     printf("\n\t*Insira as letras da Matriz*\n\n");
@@ -75,8 +69,8 @@ void leia(){
     menuPrincipal();
 }
 
-void imprima();
 void imprima(){
+
     system("cls");
     printf("\n\tMATRIZ:\n");
     printf("\t");
@@ -102,23 +96,24 @@ void imprima(){
 		}
     printf("\n");
 	}
-    menuPrincipal();  
+    menuPrincipal();
 }
 
-void leiaCad();
 void leiaCad(){
+
+
     printf("\n\t-----------------------------------");
     printf("\n\tInsira a palavra a ser encontrada: ");
 	fflush(stdin);
-    scanf("%s", palavra); 
+    scanf("%s", palavra);
 
     printf("\n\n\t!!!Pressione qualquer tecla para retornar ao menu principal!!!\n");
     system("pause>null");
     menuPrincipal();
 }
 
-void pesquisa();
 void pesquisa(){
+
     printf("\n\t==========Escolha a direcao da pesquisa==========\n\n");
     printf("\t1 - Esquerda --> Direita\n");
     printf("\t2 - Direita --> Esquerda\n");
@@ -126,11 +121,11 @@ void pesquisa(){
     printf("\t4 - Baixo --> Cima\n");
 	printf("\t0 - Retornar ao menu principal\n\n");
 	printf("\tEscolha uma opcao: ");
-    
+
     scanf("%d", &opPesquisa);
 
     switch (opPesquisa){
-        
+
 		case 1:
 			esqdir(palavra);
 		break;
@@ -142,48 +137,49 @@ void pesquisa(){
         case 3:
             cimbaix(palavra);
         break;
-        
+
         case 4:
             baixcim(palavra);
-        break;  
+        break;
 
         case 0:
         	printf("\n\n\t!!!Pressione qualquer tecla para retornar ao menu principal!!!\n");
             system("pause>null");
-            menuPrincipal(); 
-        break; 
-        
+            menuPrincipal();
+        break;
+
         default:
             printf("\nValor invalido\n");
         break;
     }
-}	
+}
 
-void matTeste();
 void matTeste(){
-    mat[0][0]='a'; mat[0][1]='b'; mat[0][2]='a'; mat[0][3]='c'; mat[0][4]='a'; mat[0][5]='x'; mat[0][6]='a'; mat[0][7]='b'; mat[0][8]='a'; mat[0][9]='a';
-	mat[1][0]='c'; mat[1][1]='b'; mat[1][2]='y'; mat[1][3]='f'; mat[1][4]='a'; mat[1][5]='z'; mat[1][6]='a'; mat[1][7]='a'; mat[1][8]='a'; mat[1][9]='l'; 
-	mat[2][0]='i'; mat[2][1]='t'; mat[2][2]='a'; mat[2][3]='e'; mat[2][4]='s'; mat[2][5]='t'; mat[2][6]='e'; mat[2][7]='r'; mat[2][8]='o'; mat[2][9]='a';
-	mat[3][0]='p'; mat[3][1]='i'; mat[3][2]='r'; mat[3][3]='a'; mat[3][4]='v'; mat[3][5]='e'; mat[3][6]='r'; mat[3][7]='o'; mat[3][8]='f'; mat[3][9]='f';
-	mat[4][0]='o'; mat[4][1]='z'; mat[4][2]='t'; mat[4][3]='a'; mat[4][4]='z'; mat[4][5]='m'; mat[4][6]='a'; mat[4][7]='d'; mat[4][8]='i'; mat[4][9]='f';
-	mat[5][0]='i'; mat[5][1]='x'; mat[5][2]='q'; mat[5][3]='m'; mat[5][4]='u'; mat[5][5]='a'; mat[5][6]='l'; mat[5][7]='a'; mat[5][8]='l'; mat[5][9]='i';
-	mat[6][0]='t'; mat[6][1]='c'; mat[6][2]='w'; mat[6][3]='n'; mat[6][4]='u'; mat[6][5]='s'; mat[6][6]='u'; mat[6][7]='l'; mat[6][8]='g'; mat[6][9]='n';
-	mat[7][0]='t'; mat[7][1]='e'; mat[7][2]='s'; mat[7][3]='t'; mat[7][4]='e'; mat[7][5]='d'; mat[7][6]='e'; mat[7][7]='l'; mat[7][8]='i'; mat[7][9]='n';
-	mat[8][0]='h'; mat[8][1]='o'; mat[8][2]='m'; mat[8][3]='a'; mat[8][4]='r'; mat[8][5]='o'; mat[8][6]='b'; mat[8][7]='o'; mat[8][8]='b'; mat[8][9]='a';
-	mat[9][0]='k'; mat[9][1]='o'; mat[9][2]='a'; mat[9][3]='z'; mat[9][4]='z'; mat[9][5]='z'; mat[9][6]='a'; mat[9][7]='m'; mat[9][8]='a'; mat[9][9]='l';
+
+    mat[0][0]='G'; mat[0][1]='A'; mat[0][2]='O'; mat[0][3]='O'; mat[0][4]='E'; mat[0][5]='F'; mat[0][6]='S'; mat[0][7]='B'; mat[0][8]='A'; mat[0][9]='B';
+	mat[1][0]='X'; mat[1][1]='T'; mat[1][2]='D'; mat[1][3]='L'; mat[1][4]='Y'; mat[1][5]='U'; mat[1][6]='A'; mat[1][7]='R'; mat[1][8]='B'; mat[1][9]='R';
+	mat[2][0]='O'; mat[2][1]='L'; mat[2][2]='A'; mat[2][3]='S'; mat[2][4]='A'; mat[2][5]='P'; mat[2][6]='T'; mat[2][7]='M'; mat[2][8]='E'; mat[2][9]='R';
+	mat[3][0]='A'; mat[3][1]='E'; mat[3][2]='S'; mat[3][3]='A'; mat[3][4]='E'; mat[3][5]='G'; mat[3][6]='O'; mat[3][7]='Z'; mat[3][8]='I'; mat[3][9]='N';
+	mat[4][0]='P'; mat[4][1]='T'; mat[4][2]='C'; mat[4][3]='R'; mat[4][4]='U'; mat[4][5]='Z'; mat[4][6]='E'; mat[4][7]='I'; mat[4][8]='R'; mat[4][9]='O';
+	mat[5][0]='C'; mat[5][1]='I'; mat[5][2]='S'; mat[5][3]='E'; mat[5][4]='G'; mat[5][5]='U'; mat[5][6]='N'; mat[5][7]='D'; mat[5][8]='A'; mat[5][9]='X';
+	mat[6][0]='A'; mat[6][1]='C'; mat[6][2]='C'; mat[6][3]='A'; mat[6][4]='M'; mat[6][5]='P'; mat[6][6]='E'; mat[6][7]='A'; mat[6][8]='O'; mat[6][9]='X';
+	mat[7][0]='O'; mat[7][1]='O'; mat[7][2]='V'; mat[7][3]='O'; mat[7][4]='E'; mat[7][5]='M'; mat[7][6]='E'; mat[7][7]='L'; mat[7][8]='H'; mat[7][9]='O';
+	mat[8][0]='A'; mat[8][1]='C'; mat[8][2]='P'; mat[8][3]='A'; mat[8][4]='L'; mat[8][5]='T'; mat[8][6]='O'; mat[8][7]='C'; mat[8][8]='A'; mat[8][9]='S';
+	mat[9][0]='G'; mat[9][1]='A'; mat[9][2]='N'; mat[9][3]='H'; mat[9][4]='O'; mat[9][5]='U'; mat[9][6]='B'; mat[9][7]='O'; mat[9][8]='N'; mat[9][9]='I';
+	mat[9][0]='G'; mat[9][1]='o'; mat[9][2]='a'; mat[9][3]='z'; mat[9][4]='z'; mat[9][5]='z'; mat[9][6]='a'; mat[9][7]='m'; mat[9][8]='a'; mat[9][9]='l';
     imprima();
 }
 
-void esqdir(char palavra[]);
 void esqdir(char palavra[]){
+
     int naoEncontrada = 0;
 
     tam = strlen(palavra);
 
     for (k = 0; k < max; k++)
-    {  
+    {
         for (l = 0; l < max; l++)
-        {    
+        {
             int l1 = 0;
             while(mat[k][l+l1] == palavra[l1] && mat[l][l+l1] != '\0' && l1 != tam && (l+l1) != max)
             {
@@ -195,9 +191,9 @@ void esqdir(char palavra[]){
                 }
 
             }
-              
+
         }
-             
+
 	}
 
     if(naoEncontrada==0)
@@ -206,16 +202,14 @@ void esqdir(char palavra[]){
     }
 }
 
-void diresq(char palavra[]);
 void diresq(char palavra[]){
     int naoEncontrada = 0;
-
     tam = strlen(palavra);
 
     for (o = 0; o < max; o++)
-    {  
+    {
         for (p = max-1; p > -1; p--)
-        {    
+        {
             int cont = 0;
             while(mat[o][p-cont] == palavra[cont] && cont != tam && (p - cont) != -1)
             {
@@ -226,87 +220,66 @@ void diresq(char palavra[]){
                     naoEncontrada++;
                 }
             }
-              
+
         }
-             
+
 	}
 
     if(naoEncontrada == 0)
     {
         printf("\n\tPalavra nao encontrada");
     }
+
+
 }
 
-void cimbaix(char palavra[]);
 void cimbaix(char palavra[]){
     int naoEncontrada = 0;
-    //printf("\n%s\n",palavra);
     tam = strlen(palavra);
-    //printf("\ntamanho antes: %d\n",tam);
     for (int q = 0; q < max; q++)
-    { 
+    {
         for (int r = 0; r < max; r++)
-        {    
-            //printf("linc %d coli %d\n", q,r);
+        {
             int cont = 0;
-            //printf("\ntam dps: %d\n",tam);
             while(mat[r + cont][q] == palavra[cont] && mat[r + cont][q] != '\0' && cont != tam && (r + cont) != max)
             {
-                //printf("\nletra: %c\n", mat[q][r]);
-                //printf("\nDentro while\n");
                 cont++;
-                //printf("\ntamanho: %d\n",tam);
                 if(cont == tam)
                 {
                     printf("\n\n\tPalavra '%s' ENCONTRADA!!!\n\tCim --> Baix: lin: %d col: %d\n", palavra, q,r);
                     naoEncontrada++;
-                    //printf("\ndentro if\n");
                 }
-                //printf("\nfora do if\n");
-
             }
-              
         }
-             
+
 	}
 
     if(naoEncontrada==0)
     {
         printf("\n\tPalavra nao encontrada");
     }
-}
+ }
 
-void baixcim(char palavra[]);
-void baixcim(char palavra[]){
+ void baixcim(char palavra[]){
     int naoEncontrada = 0;
-    //printf("\n%s\n",palavra);
     tam = strlen(palavra);
-    //printf("\ntamanho antes: %d\n",tam);
     for (int s = 0; s < max; s++)
-    {  
+    {
         for (int t = max-1; t > -1; t--)
-        {    
-            //printf("linc %d coli %d\n", s,t);
+        {
             int cont = 0;
-            //printf("\ntam dps: %d\n",tam);
             while(mat[t - cont][s] == palavra[cont] && cont != tam && (t - cont) != max)
             {
-                //printf("\nletra: %c\n", mat[s][t]);
-                //printf("\nDentro while\n");
                 cont++;
-                //printf("\ntamanho: %d\n",tam);
                 if(cont == tam)
                 {
                     printf("\n\n\tPalavra '%s' ENCONTRADA!!!\n\tBaix --> Cim: lin: %d col: %d\n", palavra, t,s);
                     naoEncontrada++;
-                    //printf("\ndentro if\n");
                 }
-                //printf("\nfora do if\n");
-
             }
-              
+
         }
-             
+
 	}
 
     if(naoEncontrada==0)
@@ -315,9 +288,3 @@ void baixcim(char palavra[]){
     }
 }
 
-int main(){
-
-menuPrincipal();
-
-return 0;
-}
